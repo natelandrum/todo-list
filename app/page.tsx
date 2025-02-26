ce85eb174ad8c36e309a28faaa73d828533f7ef9
natelandrum <nate178dawg@gmail.com>
2025-02-26 12:15:51 -0600

    Fix non-null assertion

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,22 @@ import { PrismaTask } from "@/lib/definitions";
 export default async function Home() {  
   const session = await auth();
   if (!session) redirect("/signin");
-  const tasks: PrismaTask[] = await getTasks(session?.user?.email!);
+  if (!session.user) return null;
+  let tasks: PrismaTask[] = [];
+  if (session.user.email) {
+    tasks = await getTasks(session?.user?.email);
+  }
 
   return (
+    tasks.length === 0 ? (
     <div className="container mx-auto p-4">
       <h1 className="text-4xl font-bold">Tasks</h1>
       <TaskList tasks={tasks} />
     </div>
+    ) : (
+      <div className="container mx-auto p-4">
+        <h1 className="text-4xl font-bold">No tasks found</h1>
+      </div>
+    )
   );
 }
