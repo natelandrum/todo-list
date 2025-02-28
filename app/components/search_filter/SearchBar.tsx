0a68de67a037f5e74fd0859ddcd8d4977cae0dd6
natelandrum <nate178dawg@gmail.com>
2025-02-28 13:37:46 -0600

    Fix search bar label

--- app/components/search_filter/SearchBar.tsx
+++ app/components/search_filter/SearchBar.tsx
@@ -10,17 +10,18 @@ export default function SearchBar({
   setSearchTerm,
 }: SearchBarProps) {
   return (
-    <label htmlFor="search" className="sr-only">
-      Search
-      <input
-        type="text"
-        id="search"
-        name="search"
-        placeholder="Search tasks..."
-        className="w-full md:max-w-[50%] p-4 h-10 rounded-3xl text-black"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-    </label>
+    <>
+      <label htmlFor="search" className="sr-only"></label>
+        Search
+        <input
+          type="text"
+          id="search"
+          name="search"
+          placeholder="Search tasks..."
+          className="w-full md:max-w-[50%] p-4 h-10 rounded-3xl text-black"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+    </>
   );
 }
