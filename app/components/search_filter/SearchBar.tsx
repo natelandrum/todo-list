b45aa9834c514741d6b8294379a2635883232759
natelandrum <nate178dawg@gmail.com>
2025-02-28 14:00:53 -0600

    Update search label

--- app/components/search_filter/SearchBar.tsx
+++ app/components/search_filter/SearchBar.tsx
@@ -11,17 +11,18 @@ export default function SearchBar({
 }: SearchBarProps) {
   return (
     <>
-      <label htmlFor="search" className="sr-only"></label>
+      <label className="sr-only" htmlFor="search">
         Search
-        <input
-          type="text"
-          id="search"
-          name="search"
-          placeholder="Search tasks..."
-          className="w-full md:max-w-[50%] p-4 h-10 rounded-3xl text-black"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+      </label>
+      <input
+        type="text"
+        id="search"
+        name="search"
+        placeholder="Search tasks..."
+        className="w-full md:max-w-[50%] p-4 h-10 rounded-3xl text-black"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
     </>
   );
 }
